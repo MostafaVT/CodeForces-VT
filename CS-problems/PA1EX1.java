095f89df3f6d84d32be2ce7b5181920f3b07052e
MostafaVT <58429502+MostafaVT@users.noreply.github.com>
2022-08-27 05:52:51 +0200

    Update PA1EX1.java

--- CS-problems/PA1EX1.java
+++ CS-problems/PA1EX1.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class PA1EX1{
+public class PA1EX1{
     public class LinearArray{
         int itemCount;
         int array[];
@@ -8,10 +8,52 @@ class PA1EX1{
             array = new int[maxSize];
         }
         public void insertLast(int x){
-            
+            if(itemCount<array.length){
+                array[itemCount++]=x;
+            }else{
+                System.out.println("Array is Full");
+            }
+        }
+        public void insertFirst(int x) {
+            if(itemCount<array.length){
+                for(int i=itemCount;i>0;i--) {
+                    array[i]=array[i-1];
+                }
+                array[0]=x;
+                itemCount=x;    //wot??
+            }else{
+                System.out.println("Array is Full");
+            }
+        }
+        public int linearSearch(int x) {
+            for(int i=0;i<itemCount;i++) {
+                if(array[i]==x) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void delete(int x) {
+            int pos = this.linearSearch(x); //why this.?
+            if(pos == -1) {
+                System.out.println("NOT FOUND!");
+            }else {
+                for(int i=pos;i<itemCount-1;i++) {
+                    array[i]=array[i+1];
+                }
+                itemCount--;
+            }
+        }
+        public void displayArray() {
+            System.out.println("Array: ");
+            for(int i=0;i<itemCount;i++) {
+                System.out.println(array[i] + " ");
+            }
+            System.out.println();
         }
     }
     public static void main(String[] args){
+        
     
     }
 }
